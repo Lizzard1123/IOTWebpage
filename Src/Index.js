3be482d04c3c134d40fc5a4abe03465d85ae07ba
Lizzard1123 <ethanlizzard1123@gmail.com>
2020-08-02 13:56:22 -0400

    Working sendFile

--- Src/Index.js
+++ Src/Index.js
@@ -1,41 +1,16 @@
 import fs from 'fs'
-import path from 'path'
-const __dirname = path.resolve();
 import express from 'express'
-
-const app = express()
-const port = 3000
-
+import path from 'path'
 import bodyParser from 'body-parser'
-app.use(bodyParser.json());
 
+const __dirname = path.resolve();
+const port = 3000;
+const app = express();
+app.use(bodyParser.json());
+app.use('/static', express.static('public'));
 app.route('/')
     .get((req, res) => {
-        console.log(path.join(__dirname, "Main.html"));
         res.sendFile(path.join(__dirname, "Main.html"));
     })
-    .post((req, res) => {
 
-    })
-    .put((req, res) => {
-
-    })
-    .delete((req, res) => {
-
-    })
-
-app.get('/file/js/:id', (req, res) => {
-    switch (req.params.id) {
-        case 0:
-            res.sendFile(path.join(__dirname, "GlobalJavascript.js"));
-            break;
-    }
-});
-app.get('/file/css/:id', (req, res) => {
-    switch (req.params.id) {
-        case 0:
-            res.sendFile(path.join(__dirname, "GlobalCSS.css"));
-            break;
-    }
-});
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
