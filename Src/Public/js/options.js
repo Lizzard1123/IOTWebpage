6f096600611a0d1b3ecb80050cae3088f4661c58
Lizzard1123 <ethanlizzard1123@gmail.com>
2020-08-10 13:42:10 -0400

    got local storage to work ffs

--- Src/Public/js/options.js
+++ Src/Public/js/options.js
@@ -6,46 +6,52 @@ var button_five = document.getElementsByName("button_five");
 var button_six = document.getElementsByName("button_six");
 var button_seven = document.getElementsByName("button_seven");
 var button_eight = document.getElementsByName("button_eight");
+var button_nine = document.getElementsByName("button_nine");
 var numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-var custom = [true, true, true, false, true, true, false, true, true];
+var custom = localStorage.getItem("custom").split(",");
 var normal = [true, true, true, false, true, true, false, true, true];
 
-function defultsettings() {
-    console.log("defualt");
+function addtoall() {
     for (var i = 0; i < 9; i++) {
-        if (normal[i] == false) {
+        eval(`button_${numbers[i]}[0].setAttribute("onchange", "setsettings(this)");`);
+    }
+}
+
+function defultsettings() {
+    for (var i = 0; i < 9; i++) {
+        if (normal[i] == "false") {
             eval(`button_${numbers[i]}[0].click();`);
         }
     }
 }
 
 function loadsettings() {
-    console.log("loading");
     if (custom == null) {
-        console.log("defaulttt");
         defultsettings();
     } else {
-        console.log(`getting from ${custom}`);
         for (var i = 0; i < 9; i++) {
-            if (custom[i] == false) {
+            if (custom[i] == 'false') {
                 eval(`button_${numbers[i]}[0].click();`);
             }
         }
     }
-
+    addtoall();
 }
 
 function setsettings(button) {
     var change = numbers.indexOf(button.name.split("_")[1]);
-    console.log(button.name.split("_")[1]);
     var newvarr = [];
     for (var i = 0; i < 9; i++) {
         if (i == change) {
-            newvarr.push(!custom[i]);
+            if (custom[i] == "false") {
+                newvarr.push('true');
+            } else {
+                newvarr.push('false');
+            }
         } else {
             newvarr.push(custom[i]);
         }
     }
-    console.log(newvarr);
     localStorage.setItem("custom", newvarr);
+    custom = localStorage.getItem("custom").split(",");
 }
